ca1912cbdaaaa1af87c335d3cb3977a3c245d578
Andy Shi <shi110@purdue.edu>
2015-06-24 18:28:07 -0400

    Initial Commit of Project
    Initial commit of Xcode files

---
+++ Indy Fund Fest/Indy Fund Fest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Indy Fund Fest
+//
+//  Created by Andy Shi on 6/24/15.
+//  Copyright © 2015 Indy Fund Fest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
