a3d41d05f603e35b1015d372b6e39e38d5845b54
Andy Shi <shi110@purdue.edu>
2015-07-11 00:17:14 -0400

    Added basic slide out menu

---
+++ Indy Fundfest/Indy Fundfest/SliderVC.swift
@@ -0,0 +1,49 @@
+//
+//  SliderVC.swift
+//  Indy Fundfest
+//
+//  Created by Andy Shi on 7/1/15.
+//  Copyright (c) 2015 Indy Fundfest. All rights reserved.
+//
+
+import UIKit
+
+class SliderVC: UIViewController, ENSideMenuDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Move next line to viewWillAppear functon if you store your view controllers
+        self.sideMenuController()?.sideMenu?.delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - ENSideMenu Delegate
+    func sideMenuWillOpen() {
+        print("sideMenuWillOpen")
+    }
+    
+    func sideMenuWillClose() {
+        print("sideMenuWillClose")
+    }
+    /*
+    func sideMenuShouldOpenSideMenu() -> Bool {
+        println("sideMenuShouldOpenSideMenu")
+        return false
+    }
+    */
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Indy Fundfest/Indy Fundfest/ViewController.swift
+++ Indy Fundfest/Indy Fundfest/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    @IBOutlet var scroller: UIScrollView!
-
+class ViewController: UIViewController, ENSideMenuDelegate  {
+    @IBOutlet var scrollView: UIScrollView!
+    
     override func viewDidLoad() {        
         super.viewDidLoad()
-        scroller.contentSize.height = 1000
+      //  scrollView.contentSize.height = 1000 // change programmatically
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func toggleSideMenu(sender: AnyObject) {
+          toggleSideMenuView()
+    }
+    // MARK: - ENSideMenu Delegate
+    func sideMenuWillOpen() {
+        print("sideMenuWillOpen")
+    }
+    
+    func sideMenuWillClose() {
+        print("sideMenuWillClose")
+    }
+    
+    func sideMenuShouldOpenSideMenu() -> Bool {
+        print("sideMenuShouldOpenSideMenu")
+        return true
     }
 
 
